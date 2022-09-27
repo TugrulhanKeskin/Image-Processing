1f8d93c7fa5b4b415060cb6f34c43f0f2e49666a
TugrulhanKeskin <hankeskin.bs@gmail.com>
2022-09-27 23:08:20 +0300

    odev

---
+++ 14_opencv_görüntü_işleme ödevi/OpenCV ile Görüntü İşleme Ödevi.py
@@ -0,0 +1,50 @@
+# opencv kütüphanesini içe aktaralım
+
+# resmi çizdirelim
+
+# resmin boyutuna bakalım
+
+
+# ...
+
+# resmi 4/5 oranında yeniden boyutlandıralım ve resmi çizdirelim
+# ...
+# ...
+
+# orijinal resme bir yazı ekleyelim mesela "kopek" ve resmi çizdirelim
+# ...
+# ...
+
+# orijinal resmin 50 threshold değeri üzerindekileri beyaz yap altındakileri siyah yapalım, 
+# binary threshold yöntemi kullanalım ve resmi çizdirelim
+# ...
+# ...
+
+# orijinal resme gaussian bulanıklaştırma uygulayalım ve resmi çizdirelim
+# ...
+# ...
+
+# orijinal resme Laplacian  gradyan uygulayalım ve resmi çizdirelim
+# ...
+# ...
+
+# orijinal resmin histogramını çizdirelim
+# ...
+# ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
